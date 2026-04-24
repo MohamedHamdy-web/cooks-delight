a541f0c14f54e580c46221aa51579d0ce92e573e
Mohamed Hamdy <moh04591@gmail.com>
2026-04-24 06:50:25 +0300

    add recipe by id page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login/Login";
 import Recipes from "./pages/Recipes/Recipes";
 import Signup from "./pages/Signup/Signup";
 import Verify from "./pages/Verify/Verify";
+import RecipeDetails from "./pages/RecipeDetails/RecipeDetails";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
-        <Route path="/recipes/:categoryId" element={<Recipes />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/verify" element={<Verify />} />
